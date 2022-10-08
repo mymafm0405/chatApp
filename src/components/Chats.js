84de740657c9ca4654259565b7b2f5a101390858
Mahmoud <mymafm0405@gmail.com>
2022-10-08 19:23:01 +0300

    some edit

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Chats() {
   return (
-    <div>Chats</div>
-  )
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=300"
+          alt="Photo"
+        />
+        <div className="userChatInfo">
+          <span>John</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 }
