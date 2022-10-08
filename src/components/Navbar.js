e2c69433179eb4578f889ddeda8119604be930f0
Mahmoud <mymafm0405@gmail.com>
2022-10-08 18:58:02 +0300

    Adding more and more

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <span className="logo">Mahmoud</span>
+      <div className="user">
+        <img
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=300"
+          alt="Photo"
+        />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className='search'>
+        <div className="searchForm">
+            <input type="text" placeholder='Find a user' />
+            <div className="userChat">
+                <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=300" alt="Photo" />
+                <div className="userChatInfo">
+                    <span>John</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
